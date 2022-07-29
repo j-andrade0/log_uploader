9576e131bcb28df5f0743e1bed7e192cd09c7c35
joseantonio.com <andradejoseantonio0@gmail.com>
2022-07-28 22:09:22 -0300

    remoção de método não utilizado - searchByUserAgent

--- src/main/java/keyrus/desafio_crud/model/repositories/LogRepository.java
+++ src/main/java/keyrus/desafio_crud/model/repositories/LogRepository.java
@@ -13,9 +13,6 @@ public interface LogRepository extends CrudRepository<Log, Integer> {
 	@Query("SELECT l FROM Log l WHERE l.userAgent LIKE %:userAgent%")
 	public Iterable<Log> searchByUserAgentLike(@Param("userAgent") String userAgent);
 	
-	@Query("SELECT l FROM Log l WHERE l.userAgent = 'userAgent'")
-	public Optional<Log> searchByUserAgent(@Param("userAgent") String userAgent);
-	
 	@Query("SELECT l FROM Log l WHERE l.ip LIKE %:ip%")
 	public Iterable<Log> searchByIp(@Param("ip") String ip);
 }
